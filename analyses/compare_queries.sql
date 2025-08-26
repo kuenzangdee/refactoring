1aec5cd52f6bf443e0a0bdf7ac2e7fa882f0ebe8
Kuenzang Dorji <kenzangdee90@gmail.com>
2025-08-26 07:16:34 +0000

    import CTE

--- analyses/compare_queries.sql
+++ analyses/compare_queries.sql
@@ -1,6 +1,6 @@
 {% set old_etl_relation=ref('customer_orders') %} 
 
-{% set dbt_relation=ref('fct_customer_orders') %}  {{ 
+{% set dbt_relation=ref('fct_customer__orders') %}  {{ 
 
 audit_helper.compare_relations(
         a_relation=old_etl_relation,
